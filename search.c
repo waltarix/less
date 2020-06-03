c8621fd6ef8c50ecfdd629b1e30b7577c5d08e80
waltari <gitting.crew@gmail.com>
2020-06-03 14:47:37 +0900

    custom

--- search.c
+++ search.c
@@ -376,6 +376,15 @@ public void undo_search(lbool clear)
 #endif
 }
 
+/*
+ * Toggle usage state of migemo.
+ * Updates the internal search state to reflect a change in the -% flag.
+ */
+public void chg_migemo_search(void)
+{
+	clear_pattern(&search_info);
+}
+
 #if HILITE_SEARCH
 /*
  * Clear the hilite list.
