fd5422ab5d759679ec45eb5948d9ad05fdc112dd
waltari <gitting.crew@gmail.com>
2020-06-03 14:47:37 +0900

    custom

--- pattern.c
+++ pattern.c
@@ -12,10 +12,14 @@
  */
 
 #include "less.h"
+#include "migemo.h"
+#include <stdlib.h>
+#include <syslog.h>
 
 extern int caseless;
 extern int is_caseless;
 extern int utf_mode;
+extern int migemo_search;
 
 /*
  * Compile a search pattern, for future use by match_pattern.
@@ -24,7 +28,7 @@ static int compile_pattern2(char *pattern, int search_type, PATTERN_TYPE *comp_p
 {
 	if (search_type & SRCH_NO_REGEX)
 		return (0);
-  {
+	{
 #if HAVE_GNU_REGEX
 	struct re_pattern_buffer *comp = (struct re_pattern_buffer *)
 		ecalloc(1, sizeof(struct re_pattern_buffer));
@@ -77,12 +81,28 @@ static int compile_pattern2(char *pattern, int search_type, PATTERN_TYPE *comp_p
 	*comp_pattern = comp;
 #endif
 #if HAVE_PCRE2
+	char *ptr_pattern;
+	migemo *m = NULL;
+	unsigned char *migemo_pattern;
+	if (migemo_search)
+	{
+		char* migemo_dict = getenv("MIGEMO_DICT");
+		m = migemo_open(migemo_dict);
+		migemo_pattern = migemo_query(m, (const unsigned char *)pattern);
+		ptr_pattern = (char *)migemo_pattern;
+	} else
+	{
+		ptr_pattern = pattern;
+	}
 	int errcode;
 	PCRE2_SIZE erroffset;
 	PARG parg;
-	pcre2_code *comp = pcre2_compile((PCRE2_SPTR)pattern, strlen(pattern),
-			(is_caseless ? PCRE2_CASELESS : 0),
-			&errcode, &erroffset, NULL);
+	pcre2_code *comp = pcre2_compile((PCRE2_SPTR)ptr_pattern, strlen(ptr_pattern),
+			(is_caseless ? PCRE2_CASELESS : 0) | (utf_mode ? PCRE2_UTF : 0), &errcode, &erroffset, NULL);
+	if (m != NULL) {
+		migemo_release(m, migemo_pattern);
+		migemo_close(m);
+	}
 	if (comp == NULL)
 	{
 		if (show_error)
@@ -94,6 +114,19 @@ static int compile_pattern2(char *pattern, int search_type, PATTERN_TYPE *comp_p
 		}
 		return (-1);
 	}
+#if HAVE_PCRE2_JIT
+	int jitret;
+	jitret = pcre2_jit_compile(comp, PCRE2_JIT_COMPLETE);
+	if (jitret)
+	{
+		if (show_error)
+		{
+			parg.p_int = jitret;
+			error("Couldn't JIT, got '%d'", &parg);
+		}
+		return (-1);
+	}
+#endif
 	*comp_pattern = comp;
 #endif
 #if HAVE_RE_COMP
@@ -126,7 +159,7 @@ static int compile_pattern2(char *pattern, int search_type, PATTERN_TYPE *comp_p
 	if (comp == NULL)
 	{
 		/*
-		 * regcomp has already printed an error message 
+		 * regcomp has already printed an error message
 		 * via regerror().
 		 */
 		return (-1);
@@ -135,7 +168,7 @@ static int compile_pattern2(char *pattern, int search_type, PATTERN_TYPE *comp_p
 		free(*comp_pattern);
 	*comp_pattern = comp;
 #endif
-  }
+	}
 	return (0);
 }
 
@@ -387,7 +420,7 @@ static int match_pattern1(PATTERN_TYPE pattern, char *tpattern, char *line, int 
 	{
 		int flags = (notbol) ? PCRE2_NOTBOL : 0;
 		pcre2_match_data *md = pcre2_match_data_create(nsp-1, NULL);
-		int mcount = pcre2_match(pattern, (PCRE2_SPTR)line, line_len,
+		int mcount = PCRE2_MATCH_FUNCTION(pattern, (PCRE2_SPTR)line, line_len,
 			0, flags, md, NULL);
 		matched = (mcount > 0);
 		if (matched)
@@ -466,7 +499,11 @@ public char * pattern_lib_name(void)
 	return ("POSIX");
 #else
 #if HAVE_PCRE2
+#if HAVE_PCRE2_JIT
+	return ("PCRE2 (JIT)");
+#else
 	return ("PCRE2");
+#endif
 #else
 #if HAVE_PCRE
 	return ("PCRE");
